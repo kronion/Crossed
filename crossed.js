51a3464c99e4fbcdb3f9abbd5f3bb3691c3f8d5c
Collin Stedman <cstedman@princeton>
2014-02-26 01:20:28 -0500

    Massive first push because I just binged this project

---
+++ crossed.js
@@ -0,0 +1,168 @@
+var test = [['h','e','-','l','o'],['w','o','r','l','d']];
+var solution = [];
+var x;
+var y;
+var minutes;
+var seconds;
+var interval;
+
+/* Handle arrow keys */
+window.onkeydown = function (e) {
+  if (e.which == 37 && __legalSpace(y, x-1)) {
+    __unhighlight();
+    x -= 1;
+    __highlight();
+  }
+  else if (e.which == 38 && __legalSpace(y-1, x)) {
+    __unhighlight();
+    y -= 1;
+    __highlight();
+  }
+  else if (e.which == 39 && __legalSpace(y, x+1)) {
+    __unhighlight();
+    x += 1;
+    __highlight();
+  }
+  else if (e.which == 40 && __legalSpace(y+1, x)) {
+    __unhighlight();
+    y += 1;
+    __highlight();
+  }
+}
+
+/* Creates a new game board (should only be called once) */
+function start () {
+  for (var i = 0; i < test.length; i++) {
+    var tr = "<tr>";
+    for (var j = 0; j < test[i].length; j++) {
+      tr += '<td id="td' + i + j + '"><p id="' + i + j + '"></p></td>'; 
+    }
+    tr += '</tr>';
+    $('#table').append(tr);
+    solution.push([]);
+  }
+  for (var i = 0; i < test.length; i++) {
+    for (var j = 0; j < test[i].length; j++) {
+      if (test[i][j] === '-') {
+        $('#td' + i + j).css({ 'background-color': '#000' });
+        solution[i][j] = '-';
+      }
+    }
+  }
+  x = 0;
+  y = 0;
+  minutes = 0;
+  seconds = 0;
+  
+  $('#time').text(minutes + ':' + ('0' + seconds).slice(-2));
+  interval = setInterval(__updateTime, 1000);
+
+  __highlight();
+  $('#start').hide();
+  $('form[name="character"]').show();
+  $('#restart').show();
+}
+
+/* Clears all cells of the game board */
+function restart() {
+  for (var i = 0; i < test.length; i++) {
+    for (var j = 0; j < test[i].length; j++) {
+      $('#' + i + j).text($('#' + i + j).text().replace(/./g, ''));
+      if (solution[i][j] !== '-') {
+        solution[i][j] = '';
+      }
+    }
+  }
+  __unhighlight();
+  x = 0;
+  y = 0;
+  __highlight();
+
+  clearInterval(interval);
+  minutes = 0;
+  seconds = 0;
+  $('#time').text(minutes + ':' + ('0' + seconds).slice(-2));
+  interval = setInterval(__updateTime, 1000);
+
+  $('input').each(function () {
+    $(this).removeAttr('disabled');
+  });
+}
+
+function checkComplete() {
+  if (solution.length !== test.length) {
+    return false;
+  }
+  for (var i = 0; i < solution.length; i++) {
+    if (solution[i].length !== test[i].length) {
+      return false;
+    }
+    for (var j = 0; j < solution[i].length; j++) {
+      if (!__checkSpace(i, j)) {
+        return false;
+      }
+    }
+  }  
+  return true;
+}
+
+function hint() {
+  for (var i = 0; i < test.length; i++) {
+    for (var j = 0; j < test[i].length; j++) {
+      
+    }
+}
+
+function __insertChar() {
+  var character = $('#characterInput').val();
+  if (character === " " || isNaN(character)) {
+    $('#' + y + x).text($('#' + y + x).text().replace(/./g, ''));
+    $('#' + y + x).append(character);
+    $('#characterInput').val('');
+    solution[y][x] = character;
+    if (checkComplete()) {
+      clearInterval(interval);
+      $('input').each(function () {
+        $(this).attr('disabled', 'true');
+      });
+    }
+  }
+  return false;
+}
+
+function __unhighlight() {
+  $("#td" + y + x).css({ "background-color": "#fff" });
+}
+
+function __highlight() {
+  $("#td" + y + x).css({ "background-color": "#e6e6e6" });
+}
+
+function __legalSpace(i,j) {
+  if (i < 0 || i >= test.length) {
+    return false;
+  }
+  if (j < 0 || j >= test[i].length) {
+    return false;
+  }
+  if (test[i][j] === '-') {
+    return false;
+  }
+  return true;
+}
+
+function __updateTime() {
+  seconds = seconds + 1;
+  if (seconds == 60) {
+    minutes += 1;
+    seconds = 0;
+  }
+  $('#time').text(minutes + ':' + ('0' + seconds).slice(-2));
+}
+
+function __checkSpace(i, j) {
+  if (test[i][j] === solution[i][j]) {
+    return true;
+  }
+  return false;
+}
